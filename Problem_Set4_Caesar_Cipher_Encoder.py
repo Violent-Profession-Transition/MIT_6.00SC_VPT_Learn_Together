bed56a931ad7e9c78f3fa426f914d94ba65b8f16
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2019-03-26 22:42:13 +0800

    Problem_Set4 part 1 and 2 done

---
+++ Problem_Set4_Caesar_Cipher_Encoder.py
@@ -0,0 +1,78 @@
+import string
+
+def build_coder(shift):
+    """
+    apply a Caesar cipher to a letter 
+    returns a dict
+    shift: int -27-27
+        Example: >>> build_coder(3)
+    {' ': 'c', 'A': 'D', 'C': 'F', 'B': 'E', 'E': 'H', 'D': 'G', 'G': 'J',
+    'F': 'I', 'I': 'L', 'H': 'K', 'K': 'N', 'J': 'M', 'M': 'P', 'L': 'O',
+    'O': 'R', 'N': 'Q', 'Q': 'T', 'P': 'S', 'S': 'V', 'R': 'U', 'U': 'X',
+    'T': 'W', 'W': 'Z', 'V': 'Y', 'Y': 'A', 'X': ' ', 'Z': 'B', 'a': 'd',
+    'c': 'f', 'b': 'e', 'e': 'h', 'd': 'g', 'g': 'j', 'f': 'i', 'i': 'l',
+    'h': 'k', 'k': 'n', 'j': 'm', 'm': 'p', 'l': 'o', 'o': 'r', 'n': 'q',
+    'q': 't', 'p': 's', 's': 'v', 'r': 'u', 'u': 'x', 't': 'w', 'w': 'z',
+    'v': 'y', 'y': 'a', 'x': ' ', 'z': 'b'}
+    (The order of the key-value pairs may be different.)
+    """
+    upper_letters = " " + string.ascii_uppercase
+    lower_letters = " " + string.ascii_lowercase
+    result = {}
+    for i in upper_letters:
+        index = upper_letters.index(i)
+        #if shift < 0:
+
+        if 0 <= index + shift < len(upper_letters):
+            result[i] = upper_letters[index + shift]
+        else:
+            result[i] = upper_letters[abs(len(upper_letters) - abs(index + shift))]
+    for i in lower_letters:
+        index = lower_letters.index(i)
+        if 0 <= index + shift < len(lower_letters):
+            result[i] = lower_letters[index + shift]
+        else:
+            result[i] = lower_letters[abs(len(lower_letters) - abs(index + shift))]
+    return result
+
+def build_encoder(shift):
+    """Returns a dict that can be used to encode a plain text.
+    shift: 0 <= int < 27
+    returns: dict
+    """
+    return build_coder(shift)
+
+def build_decoder(shift):
+    """Returns a dict that can be used to decode an encrypted text
+    shift: 0 <= int < 27
+    returns: dict
+    """
+    return build_coder(-shift)
+
+def apply_coder(text, coder):
+    """Applies the coder to the text. Returns the encoded text.
+     text: string
+    coder: dict with mappings of characters to shifted characters
+    returns: text after mapping coder chars to original text
+    Example:
+    >>> apply_coder("Hello, world!", build_encoder(3))
+    'Khoor,czruog!'
+    >>> apply_coder("Khoor,czruog!", build_decoder(3))
+    'Hello, world!'
+    """
+    # NOTE: python str is immutable
+    text_copy = list(text)
+    result = []
+    for letter in text_copy:
+        result.append(coder.get(letter, letter))
+    return ''.join(result)
+
+def apply_shift(text, shift):
+    """Given a text, returns a new text Caesar shifted by the given shift
+ offset.
+    The empty space counts as the 27th letter
+    apply_shift('This is a test.', 8)
+    'Apq hq hiham a.'
+    """
+    res = apply_coder(text, build_encoder(8))
+    return res
---
+++ Problem_Set4_Code_breaking.py
@@ -0,0 +1,93 @@
+"""She sends you emails, but they’re all encrypted with the Caesar cipher!
+The problem is, you don’t know which shift key she is using. 
+The good news is, you know your
+friend only speaks and writes English words
+So if you can write a program to find the decoding
+that produces the maximum number of words, you can probably find the right decoding"""
+
+from Problem_Set4_Caesar_Cipher_Encoder import *
+
+
+WORDLIST_FILENAME = "Problem_Set4_words.txt"
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r')
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = line.split()
+    print("  ", len(wordlist), "words loaded.")
+    return wordlist
+
+wordlist = load_words()
+
+def is_word(wordlist, word):
+    """
+    Determines if word is a valid word.
+
+    wordlist: list of words in the dictionary.
+    word: a possible word.
+    returns True if word is in wordlist.
+
+    Example:
+    >>> is_word(wordlist, 'bat') returns
+    True
+    >>> is_word(wordlist, 'asdf') returns
+    False
+    """
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in wordlist
+
+# (end of helper code)
+# -----------------------------------
+
+
+
+def find_best_shift(wordlist, text):
+    """
+    Decrypts the encoded text and returns the plaintext. 
+    text: string
+    returns: 0 <= int 27
+    find_best_shift(wordlist, 'Pmttw,hdwztl!') => 8
+    """
+    most_matches = 0
+
+    shift = 0
+
+    for guess_shift in range(27):
+        num_matches = 0
+        decoded_text = apply_coder(text, build_decoder(guess_shift))
+        print("now decoded_text is: ", decoded_text)
+        decoded_text_cleaned = decoded_text \
+                                    .replace("!", " ") \
+                                    .replace(",", " ") \
+                                    .replace(".", " ") \
+                                    .replace("?", " ") \
+                                    .replace(":", " ") \
+                                    .replace(";", " ") \
+                                    .replace("\"", " ") \
+                                    .replace("'", " ")
+        print("now decoded_text_cleaned is: ", decoded_text_cleaned)
+        decoded_text_list = decoded_text_cleaned.split(" ")
+        print("now decoded_text_list is: ", decoded_text_list)
+        for word in decoded_text_list:
+            if is_word(wordlist, word):
+                num_matches += 1
+        if num_matches > most_matches:
+            most_matches = num_matches
+            shift = guess_shift
+    
+    return shift
+    
